5a24fb6575d7d9bdac864cf3e82b24bbfdfdebe1
ceboola <mrcarrr@gmail.com>
2017-08-17 13:27:06 +0200

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export {WiadomosciGrid} from './WiadomosciGrid';
+export {TechGrid} from './TechGrid';
+export {GwiazdyGrid} from './GwiazdyGrid';
+export {Rehydrate} from './Rehydrated';
+export {WiadomosciSingle} from './WiadomosciSingle';
+export {GwiazdySingle} from './GwiazdySingle';
+export {TechSingle} from './TechSingle';
